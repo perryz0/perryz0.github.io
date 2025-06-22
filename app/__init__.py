a7222429d6703c480dbfc49e64d63e9a5470d04b
Perry Chien <peiruchien1@gmail.com>
2025-06-22 02:18:19 -0700

    feat: hobbies page with images

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="Perry Chien", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    hobby_items = [
+        {'title': 'Soccer', 'img': 'soccer.jpg'},
+        {'title': 'Music', 'img': 'music.jpg'},
+        {'title': 'Video Games', 'img': 'video_games.png'}
+    ]
+    return render_template('hobbies.html', title="Hobbies", hobbies=hobby_items, url=os.getenv("URL"))
