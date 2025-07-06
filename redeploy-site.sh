3fecbb9280fdee855573021a3b1bf1156725326c
Perry Chien <peiruchien1@gmail.com>
2025-07-05 21:11:59 -0700

    chore: deploy script fix

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Script should be ran from the `MLH-Portfolio` project root
+# Script should be ran from the `/root` dir in the Droplet
 
 
 # kill the current `portfolio` tmux
+cd ~/MLH-Portfolio
 tmux kill-session -t portfolio
 
 # first retrieve latest changes
@@ -14,4 +15,3 @@ pip install -r requirements.txt
 
 # new tmux and then start server
 tmux new-session -d -s portfolio 'cd ~/MLH-Portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
-
