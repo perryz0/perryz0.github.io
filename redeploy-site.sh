3689e6b1dcc2419b999aebab43f5bd38d2bbb7b2
Perry Chien <peiruchien1@gmail.com>
2025-07-14 00:48:12 -0700

    refactor: redeploy script switch to systemd from tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 # Script should be ran from the `/root` dir in the Droplet
-
-
-# kill the current `portfolio` tmux
-cd ~/MLH-Portfolio
-tmux kill-session -t portfolio
+# Current setup is using systemd to run the Flask app
 
 # first retrieve latest changes
+cd ~/MLH-Portfolio
 git fetch && git reset origin/main --hard
 
 # enter venv and update dependencies
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-# new tmux and then start server
-tmux new-session -d -s portfolio 'cd ~/MLH-Portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+# restart systemd service
+sudo systemctl restart myportfolio
