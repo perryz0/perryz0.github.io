911817d2cb9b2cd902557c9f4c5cbd9e0bfbade0
Perry Chien <peiruchien1@gmail.com>
2025-08-27 13:30:14 -0700

    chore: update work

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,16 +1,22 @@
 import type { Item } from '../components/Accordion.astro';
 
 export const experiences: Item[] = [
-  {
-    title: "Production Engineer Fellow",
-    subtitle: "@ Meta",
-    // period: "2025",
-    body: "DevOps, Systems, Infrastructure (Summer 2025)"
-  },
-  {
-    title: "Research",
-    subtitle: "@ UW Systems Lab", 
-    // period: "2025–present",
-    body: "Programmable scheduling abstractions"
-  }
+    {
+        title: "Incoming Software Engineer Intern",
+        subtitle: "@ Tesla",
+        // period: "2025",
+        body: "Multi-channel communications platform and some AI agents? (Winter 2026)"
+    },
+    {
+        title: "Production Engineer Fellow",
+        subtitle: "@ Meta",
+        // period: "2025",
+        body: "Systems, DevOps, Infrastructure and some remote fun (Summer 2025)"
+    },
+    {
+        title: "Research",
+        subtitle: "@ UW Systems Lab", 
+        // period: "2025–present",
+        body: "Programmable scheduling abstractions for big data workloads"
+    }
 ];
