64cb32bcb69e9f8da9520c5eef6805356bc60720
Perry Chien <peiruchien1@gmail.com>
2025-08-27 23:20:06 -0700

    fix: github pages deploy conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // astro.config.mjs
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+
 export default {
   trailingSlash: "always",
+  site: "https://perryz0.github.io",
+  base: "/",
   integrations: [tailwind({ applyBaseStyles: false }), mdx()],
 };
